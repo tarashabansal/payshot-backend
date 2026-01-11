17724d67d9fa4da5cec24af2ae022c8a3b267097
tarashabansal <tarashabansalwork@gmail.com>
2026-01-11 19:53:37 +0530

    requirements.txt updated

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://payshot.vercel.app", "https://www.payshot.entrext.in"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
